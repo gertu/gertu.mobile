27039df97cf82f8edc9c58cd90815707b74fd977
pqweb <pedromqv@gmail.com>
2014-01-26 18:04:57 +0100

    Registered Users

---
+++ src/com/example/gertu/Login.java
@@ -0,0 +1,64 @@
+package com.example.gertu;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+@SuppressLint("WrongViewCast")
+public class Login extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		Button boton = (Button) findViewById (R.id.btnAccess);
+        boton.setOnClickListener(new Button.OnClickListener()
+        
+        {
+            public void onClick(View v)
+            {
+            	EditText ed = (EditText) findViewById(R.id.edtUser);
+            	String texto = ed.getText().toString();
+            	EditText ed2 = (EditText) findViewById(R.id.edtPassword);
+            	String texto2 = ed2.getText().toString();
+            	ServiceHandler sh = new ServiceHandler();
+            	List<NameValuePair> params = new LinkedList<NameValuePair>();                    
+                params.add(new BasicNameValuePair("email", texto));
+                params.add(new BasicNameValuePair("password", texto2));
+                System.out.println("before of call");
+            	String result = sh.makeServiceCall("http://10.0.2.2:3000/api/v1/users/session", ServiceHandler.POST, params);
+                System.out.println(result);
+            	Intent i = new Intent (getApplicationContext(), RECLOGIN.class);
+            	i.putExtra("clave", texto);
+            	i.putExtra("pass", texto2);
+                startActivity(i);
+            }
+        });
+        
+        Button botonN = (Button) findViewById (R.id.btnNewUser);
+        botonN.setOnClickListener(new Button.OnClickListener()
+        
+        {
+            public void onClick(View v)
+            {
+            	
+            	
+            	Intent i = new Intent (getApplicationContext(), RECLOGIN.class);
+            	
+                startActivity(i);
+            }
+        });
+	}
+	
+}
---
+++ src/com/example/gertu/RECLOGIN.java
@@ -0,0 +1,34 @@
+package com.example.gertu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.TextView;
+
+public class RECLOGIN extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_reclogin);
+		
+		String data = "";
+		String data2 = "";
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null)
+        {
+            data = bundle.getString("clave");
+            data2 = bundle.getString("pass");
+        }
+
+        TextView tV = (TextView) findViewById(R.id.TextRecLogin);
+        tV.setText(data);
+        TextView tV2 = (TextView) findViewById(R.id.TextRecPassword);
+        tV2.setText(data2);
+	}
+
+	
+
+}
+
+
+
