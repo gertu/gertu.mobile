7dd45bb16b62ecc98d5b59243e0e60320b1a9543
pqweb <pedromqv@gmail.com>
2014-01-27 15:03:53 +0100

    Registered Users

--- src/com/example/gertu/Home.java
+++ src/com/example/gertu/Home.java
@@ -3,6 +3,9 @@ package com.example.gertu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends Activity {
 
@@ -10,6 +13,20 @@ public class Home extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        Button boton = (Button) findViewById (R.id.btnLogin);
+        boton.setOnClickListener(new Button.OnClickListener()
+        
+        {
+            public void onClick(View v)
+            {
+            	
+            	
+            	Intent i = new Intent (getApplicationContext(), Login.class);
+            	
+                startActivity(i);
+            }
+        });
     }
 
 
@@ -21,3 +38,7 @@ public class Home extends Activity {
     }
     
 }
+
+
+
+
